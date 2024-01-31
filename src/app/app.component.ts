30f89a5c24f9b0c7994a50d454450fd8fa2821a8
Juan Pablo Mato Hernandez <juanpablomato89@gmail.com>
2024-01-31 18:02:06 -0500

    FINISH DADOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dados';
+  ruta = '../assets/img'
+  dadoIzda = `${this.ruta}/dice1.png`
+  dadoDerecha = `${this.ruta}/dice4.png`
+  numero1: number = 0;
+  numero2: number = 1;
+
+  constructor() { }
+
+  tirarDados(): void {
+    this.numero1 = Math.round(Math.random() * 5) + 1;
+    this.numero2 = Math.round(Math.random() * 5) + 1;   
+
+    this.dadoIzda = `${this.ruta}/dice${this.numero1}.png`;
+    this.dadoDerecha = `${this.ruta}/dice${this.numero2}.png`;
+  }
 }
